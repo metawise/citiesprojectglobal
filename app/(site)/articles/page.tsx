71fdca786a8a5bbd0033030efe262fdef289c954
monikagupta <monikagupta796@gmail.com>
2025-03-07 13:13:49 +0530

    revert

--- app/(site)/articles/page.tsx
+++ app/(site)/articles/page.tsx
@@ -6,7 +6,7 @@ import { ARTICLES_QUERY, ARTICLES_PAGE_QUERY } from '@/queries/queries'
 import Head from '../head'
 import { getSlugsFromUrl } from '@/lib/tools'
 import { fetchData } from '@/lib/fetchData'
-export const fetchCache = 'no-store';
+
 
 const BlogPage = async () => {
   const [postData, data] = await Promise.all([fetchData(ARTICLES_QUERY), fetchData(ARTICLES_PAGE_QUERY)])
@@ -30,7 +30,7 @@ const BlogPage = async () => {
               <div className="flex flex-col lg:flex-row overflow-hidden">
                 <div>
                   <Image
-                    src={uniquePosts[0]?.featuredImage?.node?.sourceUrl || '/No_Image.jpg'}
+                    src={uniquePosts[0]?.featuredImage?.node?.link || '/No_Image.jpg'}
                     alt="Group of people"
                     className="w-full object-cover"
                     // layout="responsive"
@@ -118,7 +118,7 @@ const BlogPage = async () => {
                     <BlogCard
                       key={post.id}
                       index={index}
-                      image={post.featuredImage?.node?.sourceUrl}
+                      image={post.featuredImage?.node?.link}
                       date={formatDate}
                       title={post.title}
                       linkText="Read More"
